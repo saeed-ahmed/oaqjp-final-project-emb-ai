612d2540c6f7233cfd2d1dc97202a50c7a462586
Saeed Ahmed <saeed_amd@hotmail.com>
2026-01-15 03:18:55 -0500

    Task-8

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""This module is used to display different emotions and their scores of input text."""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,15 +6,24 @@ app = Flask('Emotion Detector')
 
 @app.route('/emotionDetector')
 def emotions_detector():
+    """This method returns different emotions on the input text"""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
-    
-    formated_response = "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}".format(response['anger'],response['disgust'], response['fear'], response['joy'], response['sadness'],response['dominant_emotion'])
-
+    if response['dominant_emotion'] is None:
+        formated_response = ' Invalid text! Please try again!'
+    else:
+        formated_response = (
+        f"For the given statement, the system response is "
+        f"'anger': {response['anger']}, 'disgust': {response['disgust']}, "
+        f"'fear': {response['fear']}, 'joy': {response['joy']} and "
+        f"'sadness': {response['sadness']}. "
+        f"The dominant emotion is {response['dominant_emotion']}"
+        )
     return formated_response
 
 @app.route('/')
 def render_index_page():
+    """display the root page"""
     return render_template('index.html')
 
 if __name__ == '__main__':
